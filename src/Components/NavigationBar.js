582695432dc07e38f8a14e3852106de2a12fd688
Crufixs <jcl.minano@gmail.com>
2022-10-27 13:40:20 +0800

    fixed navbar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -9,48 +9,64 @@ import { useLocation } from "react-router-dom";
 
 const NavigationBar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
+
   const location = useLocation();
 
   const handleScroll = () => {
     const position = window.pageYOffset;
-    console.log(position);
+    console.log("POS: "+position);
+    
     setScrollPosition(position);
   };
 
+  const handleResize = () => {
+    const width = window.innerWidth;
+    console.log("W: "+width);
+    setWindowWidth(width);
+  }
   useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    setScrollPosition(window.pageYOffset);
+
     window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener('resize', handleResize );
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <Navbar fixed="top" expand="sm" style={{ backgroundColor: scrollPosition > 100 ? Constants.PRIMARY1 : "rgb(150,210,200,0.15)" }}>
+    <Navbar fixed="top" expand="sm" style={{ backgroundColor: scrollPosition > 50 || windowWidth < 573  ? Constants.PRIMARY1 : "rgb(150,210,200,0.15)" }}>
       <Container>
-        <Navbar.Brand href="#home">
-          <p style={{fontWeight:"600"}}>Crufixs' Portfolio</p>
-          </Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-flex">
+
+            <p className="my-auto"style={{ fontWeight: "600"}}>Crufixs</p>
+
+
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav >
             <Nav.Link>
-              <Link to="/" className={"links " + (location.pathname ==="/" ? "active" : "")}>
+              <Link to="/" className={"links " + (location.pathname === "/" ? "active" : "")}>
                 Home
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/projects" className={"links " + (location.pathname ==="/projects" ? "active" : "")}>
+              <Link to="/projects" className={"links " + (location.pathname === "/projects" ? "active" : "")}>
                 Projects
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/school" className={"links " + (location.pathname ==="/school" ? "active" : "")}>
+              <Link to="/school" className={"links " + (location.pathname === "/school" ? "active" : "")}>
                 School
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/co-curriculars" className={"links " + (location.pathname ==="/co-curriculars" ? "active" : "")}>
+              <Link to="/co-curriculars" className={"links " + (location.pathname === "/co-curriculars" ? "active" : "")}>
                 Extra Curriculars
               </Link>
             </Nav.Link>
